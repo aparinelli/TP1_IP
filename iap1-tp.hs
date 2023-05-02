85733075002f92b6aece8787e334d31178b02cdf
aparinelli <alejoparinelli@gmail.com>
2023-05-02 01:32:05 -0300

    tests de catedra reincorporados

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -370,26 +370,26 @@ relacion2_3 = (usuario3, usuario2)
 relacion2_4 = (usuario2, usuario4)
 relacion3_4 = (usuario4, usuario3)
 
-publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])         -- en redA
-publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])                  -- en redA
-publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])         -- en redB
-publicacion1_4 = (usuario1, "Este es mi cuarto post", [])                           -- en redB
-publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])              -- en redB
+publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
+publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
+publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
+publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
+publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
 
-publicacion2_1 = (usuario2, "Hello World", [usuario4])                              -- en redA
-publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])                 -- en redA
+publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
 
-publicacion3_1 = (usuario3, "Lorem Ipsum", [])                                      -- en redA, en redB
-publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])                           -- en redA, en redB
-publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])    -- en redB
+publicacion3_1 = (usuario3, "Lorem Ipsum", [])
+publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
+publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
 
-publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])                -- en redA
-publicacion4_2 = (usuario4, "I am Bob", [])                                         -- en redA
-publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])     --
+publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
+publicacion4_2 = (usuario4, "I am Bob", [])
+publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
 
 
-usuariosA = [usuario1, usuario3, usuario2, usuario4]
-relacionesA = [relacion1_2, relacion3_4]
+usuariosA = [usuario1, usuario2, usuario3, usuario4]
+relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
 publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
 redA = (usuariosA, relacionesA, publicacionesA)
 
