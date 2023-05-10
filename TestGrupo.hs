50c133367ba5c1b1399a03128e59b77521cdff2a
aparinelli <alejoparinelli@gmail.com>
2023-05-10 19:06:05 -0300

    typos

--- TestGrupo.hs
+++ TestGrupo.hs
@@ -16,17 +16,17 @@ testSuite6 = test [
     ]
 
 testSuite7 = test [
-    "Caso 1: No hay publicaciones " ~: (publicacionesQueLeGustanA redE usuario1) ~?= []
-    "Caso 2: No dio likes " ~: (publicacionesQueLeGustanA redC usuario1) ~?= []
-    "Caso 3: Likes en redC " ~: (publicacionesQueLeGustanA redC usuario2) ~?= [publicacion3_1, publicacion3_3]
+    "Caso 1: No hay publicaciones " ~: (publicacionesQueLeGustanA redE usuario1) ~?= [],
+    "Caso 2: No dio likes " ~: (publicacionesQueLeGustanA redC usuario1) ~?= [],
+    "Caso 3: Likes en redC " ~: (publicacionesQueLeGustanA redC usuario2) ~?= [publicacion3_1, publicacion3_3],
     "Caso 4: Likes en redD " ~: (publicacionesQueLeGustanA redD usuario2) ~?= [publicacion3_1, publicacion3_2, publicacion3_3]
     ]
 
 testSuite8 = test [
-    "Caso 1: No hay publicaciones " ~: (lesGustanLasMismasPublicaciones redE usuario3 usuario1) ~?= True
-    "Caso 2: Uno de los dos no dio likes " ~: (lesGustanLasMismasPublicaciones redC usuario1 usuario2) ~?= False
-    "Caso 3: Dieron like a pubs distintas " ~: (lesGustanLasMismasPublicaciones redD usuario2 usuario3) ~?= False
-    "Caso 4: Dieron like a las mismas pubs " ~: (lesGustanLasMismasPublicaciones redD usuario1 usuario4) ~?= True
+    "Caso 1: No hay publicaciones " ~: (lesGustanLasMismasPublicaciones redE usuario3 usuario1) ~?= True,
+    "Caso 2: Uno de los dos no dio likes " ~: (lesGustanLasMismasPublicaciones redC usuario1 usuario2) ~?= False,
+    "Caso 3: Dieron like a pubs distintas " ~: (lesGustanLasMismasPublicaciones redD usuario2 usuario3) ~?= False,
+    "Caso 4: Dieron like a las mismas pubs " ~: (lesGustanLasMismasPublicaciones redD usuario1 usuario4) ~?= True,
     "Caso 5: Ninguno dio like " ~: (lesGustanLasMismasPublicaciones redC usuario1 usuario4) ~?= True
     ]
 
